1847d4837f6fd127200b5ca1fbe883da17413e60
Alex Kagai <alexkagai@outlook.com>
2024-01-24 13:49:14 +0300

    Finish npm modules

--- 03tut/index.js
+++ 03tut/index.js
@@ -1,1 +1,11 @@
-console.log("testing")
+console.log("Npm packages \n\n");
+
+const { format } = require("date-fns");
+const { v4:uuid4 } = require("uuid"); //Import add
+
+const currentDate = new Date();
+const dateFormat = "dd-MM-yyyy\tHH:mm:ss"
+
+console.log(format(currentDate, dateFormat));
+
+console.log(`\nUUID: ${uuid4()}`);
